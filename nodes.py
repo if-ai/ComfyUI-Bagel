9f7b475170aed946a98e9e8212f6dbcd0efe7065
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-23 15:44:34 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,1 +1,32 @@
+import os
+from copy import deepcopy
+from typing import (
+    Any,
+    AsyncIterable,
+    Callable,
+    Dict,
+    Generator,
+    List,
+    NamedTuple,
+    Optional,
+    Tuple,
+    Union,
+)
+import requests
+from io import BytesIO
+
+from PIL import Image
+import torch
+from accelerate import infer_auto_device_map, load_checkpoint_and_dispatch, init_empty_weights
+from safetensors.torch import load_file
+
+from data.transforms import ImageTransform
+from data.data_utils import pil_img2rgb, add_special_tokens
+from modeling.bagel import (
+    BagelConfig, Bagel, Qwen2Config, Qwen2ForCausalLM, SiglipVisionConfig, SiglipVisionModel
+)
+from modeling.qwen2 import Qwen2Tokenizer
+from modeling.bagel.qwen2_navit import NaiveCache
+from modeling.autoencoder import load_ae
+
 
