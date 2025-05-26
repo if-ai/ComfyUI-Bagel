39c9c3f2d018013bd7588d835619e56b82118cc3
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-26 15:02:52 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,25 @@
-from .nodes import LoadBAGELModel, Prompt, ImageGeneration
+from .nodes import LoadBAGELModel, Prompt, LoadEditImage, ImageGeneration, ImageThinkGeneration, ImageEditing, ImageThinkEditing, ImageUnderstanding
 
 NODE_CLASS_MAPPINGS = {
     "LoadBAGELModel": LoadBAGELModel,
     "Prompt": Prompt,
+    "LoadEditImage": LoadEditImage,
     "ImageGeneration": ImageGeneration,
+    "ImageThinkGeneration": ImageThinkGeneration,
+    "ImageEditing": ImageEditing,
+    "ImageThinkEditing": ImageThinkEditing,
+    "ImageUnderstanding": ImageUnderstanding,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadBAGELModel": "Load BAGEL Model",
     "Prompt": "Prompt",
+    "LoadEditImage": "Load Edit Image",
     "ImageGeneration": "Image Generation",
+    "ImageThinkGeneration": "Image Think Generation",
+    "ImageEditing": "Image Editing",
+    "ImageThinkEditing": "Image Think Editing",
+    "ImageUnderstanding": "Image Understanding",
 } 
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
